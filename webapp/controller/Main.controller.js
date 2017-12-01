6f3f29701a9a083f36979a1d43e6ac4201211a63
vlomben <nathan.vanlombeek@delawareconsulting.com>
2017-12-01 15:47:49 +0000

    1.0 Init

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("joris.ide.myquotations2MyQuotations2.controller.Main", {
+
+	});
+});
